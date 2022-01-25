5ff2099ecb0314bbbf1077a63d6e7d82da863a7a
smallyunet <dev.wangyu@gmail.com>
2022-01-25 21:47:19 +0800

    shell auto commit

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::net::TcpListener;
+use std::net::TcpStream;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    tcp_server();
+    tcp_client();
 }
+
+fn tcp_server() {
+    let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
+
+    for stream in listener.incoming() {
+        let _stream = stream.unwrap();
+
+        println!("Connection established!");
+    }
+}
+
+fn tcp_client() -> std::io::Result<()> {
+    let mut stream = TcpStream::connect("127.0.0.1:34254")?;
+
+    stream.write(&[1])?;
+    stream.read(&mut [0; 128])?;
+    Ok(())
+} 
