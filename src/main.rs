24a6095de8d198bec0e35c1696bdca605458cb0c
smallyunet <dev.wangyu@gmail.com>
2022-01-30 19:24:17 +0800

    shell auto commit

--- src/main.rs
+++ src/main.rs
@@ -3,24 +3,33 @@ use std::net::TcpStream;
 use std::io::prelude::*;
 
 fn main() {
-    tcp_server();
-    tcp_client();
 }
 
 fn tcp_server() {
     let listener = TcpListener::bind("127.0.0.1:7878").unwrap();
-
     for stream in listener.incoming() {
         let _stream = stream.unwrap();
-
         println!("Connection established!");
     }
 }
 
 fn tcp_client() -> std::io::Result<()> {
-    let mut stream = TcpStream::connect("127.0.0.1:34254")?;
-
+    let mut stream = TcpStream::connect("127.0.0.1:7878")?;
     stream.write(&[1])?;
     stream.read(&mut [0; 128])?;
     Ok(())
 } 
+
+#[cfg(test)]
+mod tests {
+
+    #[test]
+    fn tcp_server() {
+        tcp_server();
+    }
+
+    #[test]
+    fn tcp_client() {
+        let _res = tcp_client();
+    }
+}
---
+++ tests/tcp_test.rs
@@ -0,0 +1,6 @@
+// use super::*;
+
+// #[test]
+// fn server_test() {
+//     tcp_server();
+// }
